42f15f543d2bf0e6a43611892ac512d417a0cb7d
nicola <148383343+nicolawk@users.noreply.github.com>
2025-04-09 13:59:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Linq;
+
+public class BankAccount
+{
+    string name;
+    public double Balance;
+
+    public BankAccount(string Name, double _Balance)
+    {
+        name = Name;
+       _Balance = Balance;
+    }
+
+    public double Deposit(double deposit)
+    {
+        return Balance += deposit;
+    }
+
+    public double Withdraw(double withdraw)
+    {
+        if (withdraw > Balance) 
+        {
+            throw new InvalidOperationException();
+        }
+        return Balance =+ withdraw;
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿namespace TetProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void Wpłać() {
+            double depositAmount = 20;
+            double beginningBalance = 10;
+            double expected = 30;
+            BankAccount account1 = new BankAccount("Mrs. Mcay", beginningBalance);
+
+            account1.Deposit(depositAmount);
+
+            double actual = account1.Balance;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Wypłać() 
+        {
+            double withdrawAmount = 10;
+            double beginningBalance = 20;
+            double expected = 10;
+
+            BankAccount account2 = new BankAccount("Mr. Dobosz", beginningBalance);
+            
+            account2.Withdraw(withdrawAmount);
+        }
+
+        [TestMethod]
+        public void Wyplac_ThrowsException_WhenInsufficientFunds()
+        {
+            double withdrawAmount = 50;
+            double beginningBalance = 20;
+
+            BankAccount account = new BankAccount("Mr. Dobosz", beginningBalance);
+
+            Assert.ThrowsException<InvalidOperationException>(() => account.Withdraw(withdrawAmount));
+        }
+
+        [TestMethod]
+        public void pobierzSaldo()
+        {
+            double Balance = 10;
+            double expected = 10;
+
+            BankAccount account = new BankAccount("MS. Mick", Balance);
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
