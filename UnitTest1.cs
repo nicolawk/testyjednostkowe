2bd27a4bba9dfa621070129d20907ffb3b80a96a
nicola <148383343+nicolawk@users.noreply.github.com>
2025-04-13 17:53:54 +0200

    Add files via upload

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,54 +1,42 @@
-﻿namespace TetProject1
+using kalkulator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class KalkulatorTest
 {
-    [TestClass]
-    public class UnitTest1
+    private readonly Kalkulator calc = new Kalkulator();
+
+    [TestMethod]
+    public void Dodaj_Test()
     {
+        double result = calc.Dodaj(5, 3);
+        Assert.AreEqual(8, result);
+    }
 
-        [TestMethod]
-        public void Wpłać() {
-            double depositAmount = 20;
-            double beginningBalance = 10;
-            double expected = 30;
-            BankAccount account1 = new BankAccount("Mrs. Mcay", beginningBalance);
+    [TestMethod]
+    public void Odejmij_Test()
+    {
+        double result = calc.Odejmij(10, 4);
+        Assert.AreEqual(6, result);
+    }
 
-            account1.Deposit(depositAmount);
+    [TestMethod]
+    public void Pomnoz_Test()
+    {
+        double result = calc.Pomnoz(6, 7);
+        Assert.AreEqual(42, result);
+    }
 
-            double actual = account1.Balance;
-            Assert.AreEqual(expected, actual);
-        }
+    [TestMethod]
+    public void Podziel_Test()
+    {
+        double result = calc.Podziel(10, 2);
+        Assert.AreEqual(5, result);
+    }
 
-        [TestMethod]
-        public void Wypłać() 
-        {
-            double withdrawAmount = 10;
-            double beginningBalance = 20;
-            double expected = 10;
-
-            BankAccount account2 = new BankAccount("Mr. Dobosz", beginningBalance);
-            
-            account2.Withdraw(withdrawAmount);
-        }
-
-        [TestMethod]
-        public void Wyplac_ThrowsException_WhenInsufficientFunds()
-        {
-            double withdrawAmount = 50;
-            double beginningBalance = 20;
-
-            BankAccount account = new BankAccount("Mr. Dobosz", beginningBalance);
-
-            Assert.ThrowsException<InvalidOperationException>(() => account.Withdraw(withdrawAmount));
-        }
-
-        [TestMethod]
-        public void pobierzSaldo()
-        {
-            double Balance = 10;
-            double expected = 10;
-
-            BankAccount account = new BankAccount("MS. Mick", Balance);
-            double actual = account.Balance;
-            Assert.AreEqual(expected, actual);
-        }
+    [TestMethod]
+    public void Podziel_ThrowsException_WhenDividingByZero()
+    {
+        Assert.ThrowsException<DivideByZeroException>(() => calc.Podziel(5, 0));
     }
 }
